755ffe7d7f0a339fc0e9f0e1db3f67e5a299c4de
Quorbinc <56852284+Quorbinc@users.noreply.github.com>
2021-08-15 09:47:56 -0700

    Update UserCode.c
    added a comment indicating where user code starts and took out the timedelay function for the time being until it can be added in a different file later

--- UserCode.c
+++ UserCode.c
@@ -8,14 +8,6 @@
   u16 uwValue;
   u08 ubValue;
 
-void timeDelay (u32 uwDelay)
-{
-  for (int i = 0; i < uwDelay; i++)
-  {
-    nop100;
-  }
-}
-
 int main (void)
 {
   u32 uwDelay; //was a u16 changed to u32
@@ -23,34 +15,10 @@ int main (void)
   //--- Main Infinite Loop Entry Point
   while (1)
   {
-    SET_PB00;
-    timeDelay(100000);
-    CLR_PB00;
-    timeDelay(100000);
-
-    //----------below is old code I was using----------//
-    /*
-    //--- Turn On the Pacer LED pin to GND
-    CLR_PC15;
-
-    for (uwDelay = 0; uwDelay < 320000; uwDelay++) //uwdelay was 16000
-    {
-      SET_PB01;
-      nop100;
-      CLR_PB01;
-      nop100;
-    }
-
-    //--- Turn off PACER LED
-    SET_PC15;
-
-    for (uwDelay = 0; uwDelay < 320000; uwDelay++) //uwdelay was 16000
-    {
-      SET_PB01;
-      nop100;
-      CLR_PB01;
-      nop100;
-    */
-    //----------above is old code I was using----------//
+    /*=============================*/
+    /*                             */
+    /*     User code goes here     */
+    /*                             */
+    /*=============================*/
   }
 }
