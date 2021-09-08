58e6b7735938ab5a0aa4f6a0fb890c306d9aca01
aracnoid149 <ethan@fredsti.com>
2021-09-08 15:32:47 -0700

    updated title comment
    updated title comment to reflect our company's current name and which core this accommodates.

--- rtos/startup.h
+++ rtos/startup.h
@@ -1,9 +1,12 @@
 //============================================================================================
-//                                        QUORB Inc.                                                 //
+//                                        QUORB Inc.                                                 
 //                                -------------------------
 //
 //                                     C Start Up Code
 //                                     ---------------
+//
+//    This code is for use on the STM32L432 Processor Series of Micro Controllers it is
+//    It is not fully compatible with other STM32     Series Micro Controller Products
 //
 //                           Written By:  Sancar James Fredsti
 //
