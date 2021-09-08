956b9b6d731a9f1217c524fcc12f75820a1d8011
aracnoid149 <ethan@fredsti.com>
2021-09-08 14:33:34 -0700

    Update main.h

--- rtos/main.h
+++ rtos/main.h
@@ -7,7 +7,8 @@
 extern  u32 ulValue;
 extern  u16 uwValue;
 extern  u08 ubValue;
-
+extern  struct Task stGlobalTask;                           //--- Global Task for schedueling
 
 int main (void);
+void fnRecursive (struct PassData stPassData);
 
