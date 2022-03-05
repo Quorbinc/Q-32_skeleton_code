e7512ebc5144d1879399584b7f6dec211c31a4fb
aracnoid149 <ethan@fredsti.com>
2022-03-05 11:08:22 -0800

    Create timers.c

---
+++ Simplex/timers.c
@@ -0,0 +1,14 @@
+//=============================================================================================
+//                                        Quorb Inc.
+//                                -------------------------
+//
+//                         Base Code For STM32F446 Timer Functions
+//                  -----------------------------------------------------
+//
+//    This code is for use on the STM32L432 Processor Series of Micro Controllers it is
+//    It is not fully compatible with other STM32     Series Micro Controller Products
+//
+//                             Written By:  Sancar James Fredsti
+//
+//=============================================================================================
+#include "projectdefs.h"
