53d82a0f40911a121f8c60676e38c9b56768e004
aracnoid149 <ethan@fredsti.com>
2022-03-06 14:55:21 -0800

    exposed 2 functions
    uncommented void __ISB(void) and void __DSB(void)

--- Simplex/commonheader.h
+++ Simplex/commonheader.h
@@ -1810,8 +1810,8 @@
   void __WFI(void);
   void __WFE(void);
   void __SEV(void);
-//  void __ISB(void);
-//  void __DSB(void);
+  void __ISB(void);
+  void __DSB(void);
   void __DMB(void);
   void __SVC(void);
   u32  __MRS_CONTROL(void);
