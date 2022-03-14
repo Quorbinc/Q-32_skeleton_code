90494974aad0762d6db7f4e59e3c76ccf5e4793f
aracnoid149 <ethan@fredsti.com>
2022-03-14 16:58:24 -0700

    removed unused values
    removed unused variables

--- rtos/projectcode.h
+++ rtos/projectcode.h
@@ -4,9 +4,6 @@
 
 #include "projectdefs.h"
 
-extern  u32 ulValue;
-extern  u16 uwValue;
-extern  u08 ubValue;
 extern  struct Task stGlobalTask;                           //--- Global Task for schedueling
 
 int main (void);
