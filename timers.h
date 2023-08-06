e0b600af38c35d23a7519ab9345b81fdb6fe6d53
Ethan Fredsti <ethan@fredsti.com>
2023-08-06 12:53:16 -0700

    grouped functions together
    grouped functions together by the timer they use and put them under a define statement.

--- timers.h
+++ timers.h
@@ -15,34 +15,50 @@
 #ifndef TIMER_H
     #define TIMER_H
 
-    //-----Timer initializing functions-----//
-    void fnTim1Init (void);
-    void fnTim2Init (void);
-    void fnTim6Init (void);
-    void fnTim7Init (void);
-    void fnTim15Init (void);
-    void fnTim16Init (void);
+    #include "projectdefs.h"
 
-    //-----Generic timer output-----//
-    void fnTim1Out(u16 uwChange, u08 ubPin);
-    void fnTim2Out(u16 uwChange, u08 ubPin);
-    void fnTim15Out(u16 uwChange, u08 ubPin);
-    void fnTim16Out(u16 uwChange, u08 ubPin);
+    //-----Timer 1 definitions-----//
+    #ifdef TIM1_FLAG
+        void fnTim1Init (void);
+        void fnTim1Out(u16 uwChange, u08 ubPin);
+        void fnTim1In(u16 uwPeriod, u08 ubPin);
+        void fnTim1PWMOut(u16 uwLoad, u08 ubPin);
+        void fnTIM1PWMIn(u16 uwPeriod, u08 ubPin);
+    #endif
 
-    //-----generic timer input-----//
-    void fnTim1In(u16 uwPeriod, u08 ubPin);
-    void fnTim2In(u16 uwPeriod, u08 ubPin);
-    void fnTim15In(u16 uwPeriod, u08 ubPin);
-    void fnTim16In(u16 uwPeriod, u08 ubPin);
+    //-----Timer 2 definitions-----//
+    #ifdef TIM2_FLAG
+        void fnTim2Init (void);
+        void fnTim2Out(u16 uwChange, u08 ubPin);
+        void fnTim2In(u16 uwPeriod, u08 ubPin);
+        void fnTim2PWMOut(u16 uwLoad, u08 ubPin);
+        void fnTIM2PWMIn(u16 uwPeriod, u08 ubPin);
+    #endif
 
-    //-----generc PWM output-----//
-    void fnTim1PWMOut(u16 uwLoad, u08 ubPin);
-    void fnTim2PWMOut(u16 uwLoad, u08 ubPin);
-    void fnTim15PWMOut(u16 uwLoad, u08 ubPin);
-    void fnTim16PWMOut(u16 uwLoad, u08 ubPin);
+    //-----Timer 6 definitions-----//
+    #ifdef TIM6_FLAG
+        void fnTim6Init (void);
+    #endif
 
-    //-----generic PWM input-----//
-    void fnTIM1PWMIn(u16 uwPeriod, u08 ubPin);
-    void fnTIM2PWMIn(u16 uwPeriod, u08 ubPin);
-    void fnTIM15PWMIn(u16 uwPeriod, u08 ubPin);
+    //-----Timer 7 definitions-----//
+    #ifdef TIM7_FLAG
+        void fnTim7Init (void);
+    #endif
+
+    //-----Timer 15 definitions-----//
+    #ifdef TIM15_FLAG
+        void fnTim15Init (void);
+        void fnTim15Out(u16 uwChange, u08 ubPin);
+        void fnTim15In(u16 uwPeriod, u08 ubPin);
+        void fnTim15PWMOut(u16 uwLoad, u08 ubPin);
+        void fnTIM15PWMIn(u16 uwPeriod, u08 ubPin);
+    #endif
+
+    //-----Timer 16 definitions-----//
+    #ifdef TIM16_FLAG
+        void fnTim16Init (void);
+        void fnTim16Out(u16 uwChange, u08 ubPin);
+        void fnTim16In(u16 uwPeriod, u08 ubPin);
+        void fnTim16PWMOut(u16 uwLoad, u08 ubPin);
+    #endif
 #endif
